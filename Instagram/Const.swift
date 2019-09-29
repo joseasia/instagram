01e9f822cc39a3cf695c95ee2d8f031d662d8262
jose_asia <y.s.e0414@gmail.com>
2019-09-30 08:20:42 +0900

    カリキュラムの写経までDone

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 佐藤　由羽太 on 2019/09/29.
+//  Copyright © 2019 佐藤　由羽太. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
